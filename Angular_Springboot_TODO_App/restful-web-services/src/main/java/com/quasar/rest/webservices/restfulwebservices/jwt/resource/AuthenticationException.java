5d4eceae89551e72f44c3a500b53a4472688d333
Kiran Ramesh <kiran.rs2@gmail.com>
2020-09-02 10:32:35 -0400

    Deleted UserTaskManager files as these are moved to its own repository

--- Angular_Springboot_TODO_App/restful-web-services/src/main/java/com/quasar/rest/webservices/restfulwebservices/jwt/resource/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.quasar.rest.webservices.restfulwebservices.jwt.resource;
-
-public class AuthenticationException extends RuntimeException {
-	public AuthenticationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
--- Angular_Springboot_TODO_App/restful-web-services/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into todo(id,username,description,target_date,done)
-values(1001,'kiran','Learn JPA',sysdate(),false);
-
-insert into todo(id,username,description,target_date,done)
-values(1002,'kiran','Learn Hibernate',sysdate(),false);
